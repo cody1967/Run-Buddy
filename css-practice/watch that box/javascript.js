368cf8de47bba616ca87a9923161bb02fd638de6
Cody McNeill <cody@utahhydrographics.com>
2021-01-19 23:47:09 -0700

    trainers

---
+++ css-practice/watch that box/javascript.js
@@ -0,0 +1,32 @@
+
+
+function height() {
+    console.log("grow")
+    document.getElementById("box").style.height = "250px";
+}
+
+function blue() {
+    console.log("blue")
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+
+function fade() {
+    console.log("fade")
+    document.getElementById("box").style.opacity = "25%";
+}
+
+
+
+function reset() {
+    console.log("reset")  
+    document.getElementById("box").style.backgroundColor = "orangered";
+    document.getElementById("box").style.opacity = "100%";
+    document.getElementById("box").style.height = "150px";
+}
+
+
+
+
+
+
